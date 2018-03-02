e0e220d2ed29bf3dc8e01c369914b89e9bb84932
ojcarrillo <oj_carrillo@yahoo.com>
2018-03-02 13:18:15 +0000

    agregado soporte ftp

---
+++ start.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo "======================================================="
+echo "compila aplicacion"
+mvn clean package -Dmaven.test.skip=true
+echo "======================================================="
+echo "======================================================="
+echo "compila imagen docker - dk_receivermq_sap "
+docker build -t dk_receivermq_sap .
+echo "======================================================="
+echo "======================================================="
+echo "corre imagen del contenedor"
+docker run \
+ --name dk_receivermq_sap \
+ --net=backend \
+ -it dk_receivermq_sap
+echo "======================================================="
+echo "======================================================="
+echo "fin"
+
+
---
+++ stop.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker stop dk_receivermq_sap && docker rm  dk_receivermq_sap
